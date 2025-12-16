31069c380234e8d8d31de0ca320d9023e6d6508e
Rithulak <rithulak21@gmail.com>
2025-12-16 10:09:53 +0530

    program added

---
+++ prg1.py
@@ -0,0 +1,11 @@
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+c=int(input("Enter third number: "))
+if a>=b and a>=c:
+    print(f"{a} is the largest number")
+elif b>=a and b>=c:
+    print(f"{b} is the largest number")
+else:
+    print(f"{c} is the largest number")
+
+# This program finds the largest of three numbers entered by the user.
