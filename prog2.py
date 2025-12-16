8d33470cd151259f1891c8308032af403b22ed2e
Rithulak <rithulak21@gmail.com>
2025-12-16 10:31:48 +0530

    created new program

---
+++ prog2.py
@@ -0,0 +1,10 @@
+print("Hello World")
+# This program prints "Hello World" to the console.
+
+# It serves as a basic introduction to programming.
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+sum=a+b
+print("Sum of",a,"and",b,"is",sum)
+# This program calculates the sum of two numbers entered by the user.
+
